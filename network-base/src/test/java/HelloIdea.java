f7501681b455c983a945a0db3ace4597b00c76d8
landon <lvwenyong@playcrab.com>
2019-12-11 17:51:53 +0800

    init idea test

---
+++ network-base/src/test/java/HelloIdea.java
@@ -0,0 +1,5 @@
+public class HelloIdea {
+    public static void main(String[] args) {
+        System.out.println("Hello,Idea");
+    }
+}
---
+++ network-base/src/test/java/HelloJunit.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloJunit {
+
+    @Test
+    public void test() {
+        int a = 100;
+        Assert.assertEquals(a, 100);
+    }
+
+    @Test
+    public void test2() {
+        int b = 50;
+        Assert.assertEquals(b, 100);
+    }
+}
